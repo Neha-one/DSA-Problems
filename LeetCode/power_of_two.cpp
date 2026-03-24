90faacf9c3237fa62b70e2d0b305be8fe95b250c
Neha-one <justforgtb@gmail.com>
2026-03-24 23:20:03 +0530

    add these files

---
+++ LeetCode/power_of_two.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+bool isPowerOfTwo(int n)
+{
+    if (n <= 0)
+        return false;
+
+    while (n > 1)
+    {
+        if (n % 2 != 0)
+        {
+            return false; // not divisible by 2
+        }
+        n = n / 2;
+    }
+
+    return true;
+}
+
+int main()
+{
+    int n;
+    cout<<"enter number: ";
+    cin >> n;
+
+    if (isPowerOfTwo(n))
+        cout << "Yes";
+    else
+        cout << "No";
+}
