f5b55c36f4291cc9ede859a1420cb89be4ff050e
Neha-one <justforgtb@gmail.com>
2026-03-26 23:18:16 +0530

    add these file

---
+++ LeetCode/Subtract_Product_and_Sum_of_Digits.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int subtractProductAndSum(int n) {
+    int sum = 0, product = 1;
+
+    while (n != 0) {
+        int digit = n % 10;
+        sum += digit;
+        product *= digit;
+        n = n / 10;
+    }
+
+    return product - sum;
+}
+
+int main() {
+    int n;
+    cout<<"enter number: ";
+    cin >> n;
+
+    cout << subtractProductAndSum(n);
+
+    return 0;
+}
