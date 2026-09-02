5586bd71a78f2781fcceec2c808d4e4f3203ec9d
Neha-one <justforgtb@gmail.com>
2026-09-02 23:51:13 +0530

    add this dsa problem

---
+++ practiceNextDay.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void sumofarr(vector<int> &arr1, int n, vector<int> &arr2, int m, vector<int> &ans)
+{
+    int i = n - 1, j = m - 1, carry = 0;
+    while (i >= 0 && j >= 0)
+    {
+        int sum = arr1[i] + arr2[j] + carry;
+        int digit = sum % 10;
+        ans.push_back(digit);
+        carry = sum / 10;
+        i--, j--;
+    }
+    while (i >= 0)
+    {
+        int sum = arr1[i] + carry;
+        int digit = sum % 10;
+        ans.push_back(digit);
+        carry = sum / 10;
+        i--;
+    }
+    while (j >= 0)
+    {
+        int sum = arr2[j] + carry;
+        int digit = sum % 10;
+        ans.push_back(digit);
+        carry = sum / 10;
+        j--;
+    }
+    if (carry != 0)
+    {
+        ans.push_back(carry);
+    }
+}
+void print(vector<int> ans)
+{
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+}
+int main()
+{
+    vector<int> arr1 = {1, 2, 3};
+    vector<int> arr2 = { 7};
+    int n = arr1.size();
+    int m = arr2.size();
+    vector<int> ans;
+    sumofarr(arr1, n, arr2, m, ans);
+    reverse(ans.begin(), ans.end());
+    print(ans);
+    return 0;
+}
